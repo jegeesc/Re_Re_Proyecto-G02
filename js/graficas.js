d4dddf7380c6831ba54f25d6c02d2bd7b0f24816
Carles <carlesmartif@gmail.com>
2021-05-23 17:24:19 +0200

    Se puede cambiar entre tipos de graficas

--- js/graficas.js
+++ js/graficas.js
@@ -1,24 +1,27 @@
+var miGrafica;
+graficaDefault()
 //SELECTOR DE GRÁFICAS
 function obtenerTipoGrafica(tipo) {
-
     if (tipo === 'barras') {
+
         console.log("BARRAS")
-        var tipoGrafica = 'barras'
-        var ejePrincipal = ['lunes', 'martes', 'miércoles', 'jueves', 'viernes']
-        cargarGrafica(ejePrincipal)
+        var tipoGrafica = 'bar';
+        var ejePrincipal = ['lunes', 'martes', 'miércoles', 'jueves', 'viernes'];
+        cargarGrafica(ejePrincipal,tipoGrafica)
+
     } else {
         if (tipo === 'lineas') {
             console.log("LÍENAS")
-            var tipoGrafica = 'lineas'
+            var tipoGrafica = 'line'
             var ejePrincipal = ['']
-            cargarGrafica(ejePrincipal)
+            cargarGrafica(ejePrincipal,tipoGrafica)
         } else {
             console.log("ERROR")
         }
     }
 }
 //DEFINICION DE LOS DATOS DE CADA TIPO DE GRAFICA
-function graficaLineas(){
+function grafica(ejePrincipal,tipoGrafica){
     let datos = {
 
         labels: ejePrincipal,
@@ -70,32 +73,17 @@ function graficaLineas(){
         ]
     };
 
-
-}
-//Definimos los datos en el eje horizontal con los valores del array
-
-//LA GRÁFICA SE DIBUJA EN PANTALLA
-function cargarGrafica(ejePrincipal) {
-    var miGrafica;
-    //Llenamos la gráfica de datos
+    //--------------------------------------------------------
 
 
-    if(!miGrafica){
+    let ctx = document.getElementById('chart');
+    //Destuir grafica antes de crear la nueva
+    if(miGrafica){
         miGrafica.destroy();
-        Chart.destroy();
-        console.log('Definido')
-    }else  {
-        console.log('No definido')
-
     }
 
-
-    //modificamos el objeto canvas
-    let ctx = document.getElementById('chart');
-
-//mostramos la gráfica
     miGrafica = new Chart(ctx, {
-        type: 'line',
+        type: tipoGrafica,
         data: datos,
         options: {
             responsive: true,
@@ -124,92 +112,19 @@ function cargarGrafica(ejePrincipal) {
             }
         }
     });
+}
+//Definimos los datos en el eje horizontal con los valores del array
+
+//LA GRÁFICA SE DIBUJA EN PANTALLA
+function cargarGrafica(ejePrincipal,tipoGrafica) {
+
+        grafica(ejePrincipal,tipoGrafica)
+
+
 
 }
 
-//GRÁFICA 2
-
-
-let datos2 = {
-    labels: [''],
-    //Se definen los conjuntos de datos de nombre ventas y con los valores del array
-    datasets: [
-        {
-            label: 'humedad',
-            data: [100, 234, 45, 210, 430],
-            //así se rellena
-            fill: true,
-            //color de fondo
-            backgroundColor: 'rgba(117, 145, 255, 0.7)',
-            //color del borde
-            borderColor: 'rgba(92, 125, 255, 0.7)',
-            //Permite que la línea en vez de ser una recta sea una curva NO PASAR DE 1 solo decimal
-            tension: 0,
-            //Cambia la forma de los puntos
-            pointStyle: 'circle',
-            //tamaño de los puntos
-            pointRadius: 5,
-        }, { //esto es otro objeto de valores que se pondría encima de la otra
-            label: 'salinidad',
-            data: [350, 34, 267, 110, 30],
-            fill: true,
-            tension: 0,
-            backgroundColor: 'rgba(179, 252, 246, 0.7)',
-            borderColor: 'rgba(113, 217, 209, 0.7)',
-            pointStyle: 'circle',
-            pointRadius: 5,
-        }, { //esto es otro objeto de valores que se pondría encima de la otra
-            label: 'iluminación',
-            data: [20, 14, 67, 10, 30],
-            fill: true,
-            tension: 0,
-            backgroundColor: 'rgba(239, 255, 133, 0.7)',
-            borderColor: 'rgba(234, 255, 94, 0.7)',
-            pointStyle: 'circle',
-            pointRadius: 5,
-        }, { //esto es otro objeto de valores que se pondría encima de la otra
-            label: 'temperatura',
-            data: [120, 114, 167, 110, 130],
-            fill: true,
-            tension: 0,
-            backgroundColor: 'rgba(255, 99, 99, 0.7)',
-            borderColor: 'rgba(255, 77, 77, 0.7)',
-            pointStyle: 'circle',
-            pointRadius: 5,
-        }
-    ]
-};
-
-let ctx2 = document.getElementById('chart2');
-//mostramos la gráfica
-let miGrafica2 = new Chart(ctx2, {
-    type: 'bar',
-    data: datos2,
-    options: {
-        indexAxis: 'y',
-        responsive: true,
-        maintainAspectRatio: false,
-        transitions: {
-            show: {
-                animations: {
-                    x: {
-                        from: 0
-                    },
-                    y: {
-                        from: 0
-                    }
-                }
-            },
-            hide: {
-                animations: {
-                    x: {
-                        to: 0
-                    },
-                    y: {
-                        to: 0
-                    }
-                }
-            }
-        }
-    }
-});
+function graficaDefault(){
+    var ejePrincipal = [''];
+    cargarGrafica(ejePrincipal,'bar')
+}
