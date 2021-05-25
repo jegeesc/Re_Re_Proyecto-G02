103b0e17f912881c4986d70090e95859854c53af
Carles <carlesmartif@gmail.com>
2021-05-25 09:40:37 +0200

    Funciones mapa preparadas

--- js/mapas.js
+++ js/mapas.js
@@ -1,10 +1,6 @@
 let map;
 
-
-///hola
-
 function initMap() {
-    //console.log("Google Maps cargado!!");
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 39.00971602044704, lng: -0.18356627613238444},
         zoom: 15,
@@ -23,7 +19,9 @@ function initMap() {
         streetViewControl: false,
         rotateControl: false,
     });
-    //Marcadores
+    //------------------------Marcadores------------------------
+
+        //------------------------SONDA 1------------------------
     var sonda1 = new google.maps.Marker({
         position: {lat: 39.01068307409519, lng: -0.18946713596529477},
         label: "Sonda 1",
@@ -32,8 +30,12 @@ function initMap() {
 
     });
     sonda1.addListener("click", () => {
+        var contentString = crearInfoWindow('Sonda1');
+        var infowindow = crearVar(contentString,map);
         infowindow.open(map, sonda1);
     });
+
+        //------------------------SONDA 2------------------------
     var sonda2 = new google.maps.Marker({
         position: {lat: 39.012583796216596, lng: -0.18343753009972605},
         label: "Sonda 2",
@@ -43,20 +45,28 @@ function initMap() {
     sonda2.addListener("click", () => {
         infowindow.open(map, sonda2);
     });
+        //------------------------SONDA 3------------------------
     var sonda3 = new google.maps.Marker({
         position: {lat: 39.00720495033061, lng: -0.183881301062853},
         label: "Sonda 3",
         animation: google.maps.Animation.DROP,
         map: map
     });
-
+    sonda3.addListener("click", () => {
+        infowindow.open(map, sonda3);
+    });
+        //------------------------SONDA 4------------------------
     var sonda4 = new google.maps.Marker({
         position: {lat: 39.00934263744238, lng: -0.1784923340801241},
         label: "Sonda 4",
         animation: google.maps.Animation.DROP,
         map: map
     });
-    //Marcadores
+    sonda4.addListener("click", () => {
+        infowindow.open(map, sonda4);
+    });
+
+    //------------------------POLIGONOS------------------------
     let polygon = new google.maps.Polygon({
         paths: [
             {lat: 39.010661, lng: -0.189822},
@@ -71,6 +81,8 @@ function initMap() {
         fillOpacity: 0.35,
         map: map
     });
+
+
     /*Ajustar el mapa a un polígono
     let bounds = new google.maps.LatLngBounds();
     polygon.getPath().getArray().forEach(function (v) {
@@ -79,11 +91,14 @@ function initMap() {
     map.fitBounds(bounds);
     */
 
+
+function crearInfoWindow(sonda){
+    console.log('He llegado')
     //------------------------ CREA CODIGO HTML ------------------------
     const contentString =
         '<div class="iw_container">' +
             '<div class="iw_div_principal">' +
-                '<h1 class="iw_titulo">Sonda 1</h1>' +
+                '<h1 class="iw_titulo">'+ sonda + '</h1>' +
             "</div>" +
             '<div class="iw_logosytext">' +
                 '<div class="iw_iconos">' +
@@ -115,12 +130,18 @@ function initMap() {
             "</div>" +
         "</div>"
     ;
+    return contentString;
+}
 
-    const infowindow = new google.maps.InfoWindow({
-        content: contentString,
-        maxWidth: 500,
-        minWidth: 250,
-    });
+    function crearVar(contentString,map){
+
+        const infowindow = new google.maps.InfoWindow({
+            content: contentString,
+            maxWidth: 500,
+            minWidth: 250,
+        });
+    return  infowindow;
+    }
 }
 
 
