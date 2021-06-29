b6a81fbf600d267e0ab742f18f4aa336b657ef6d
Xavi <x.egea.es@gmail.com>
2021-06-29 19:01:46 +0200

    mapas_y_graficas_y_botonContactoArreglado

---
+++ src/app/js/medidasPersonalizadas.js
@@ -0,0 +1,10 @@
+
+    /*$.ajax("graficasBD.js",{
+        method:"POST",
+        data: JSON.stringify(medidasObjeto),
+        contentType: 'application/json',
+    }).done(function (){
+        console.log("Medidas Modificadas")
+    })//$.ajax() */
+
+
